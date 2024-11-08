589356d5af42c00ee149f96cafea3bbca9c00ef0
katelll16 <160080339+katelll16@users.noreply.github.com>
2024-11-08 10:42:40 -0800

    Add files via upload

---
+++ RoomService/src/test/java/com/group75/RoomService/RoomServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group75.RoomService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ UserService/src/test/java/com/group75/UserService/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group75.UserService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
