363be60c163756aba394472a8bd3f0f176e94f50
Kate Labis <101413112@georgebrown.ca>
2024-11-08 21:57:30 -0800

    main

--- RoomService/src/main/java/com/group75/RoomService/RoomServiceApplication.java
+++ RoomService/src/main/java/com/group75/RoomService/RoomServiceApplication.java
@@ -2,12 +2,12 @@ package com.group75.RoomService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class RoomServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RoomServiceApplication.class, args);
 	}
-
 }
--- UserService/src/main/java/com/group75/UserService/UserServiceApplication.java
+++ UserService/src/main/java/com/group75/UserService/UserServiceApplication.java
@@ -2,12 +2,12 @@ package com.group75.UserService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
-
 }
