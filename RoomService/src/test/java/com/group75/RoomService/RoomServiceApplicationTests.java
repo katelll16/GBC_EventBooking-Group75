2662239469d0c01ca902a306cef35acb25cd65d0
Kate Labis <101413112@georgebrown.ca>
2024-11-09 19:02:26 -0800

    main

--- RoomService/src/test/java/com/group75/RoomService/RoomServiceApplicationTests.java
+++ RoomService/src/test/java/com/group75/RoomService/RoomServiceApplicationTests.java
@@ -1,10 +1,22 @@
-package com.group75.RoomService;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
 @SpringBootTest
-class RoomServiceApplicationTests {
+public class RoomServiceApplicationTests {
+
+	static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:latest")
+			.withDatabaseName("test_db")
+			.withUsername("user")
+			.withPassword("password");
+
+	@BeforeAll
+	static void startContainer() {
+		postgres.start();
+	}
+
+	@DynamicPropertySource
+	static void databaseProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.datasource.url", postgres::getJdbcUrl);
+		registry.add("spring.datasource.username", postgres::getUsername);
+		registry.add("spring.datasource.password", postgres::getPassword);
+	}
 
 	@Test
 	void contextLoads() {
